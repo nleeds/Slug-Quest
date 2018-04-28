02f4496e1b8d07fe4c2c1580a03113faae2fc39e
nleeds <nleeds@ucsc.edu>
2018-04-28 15:17:02 -0700

    added a simple map activity

--- app/src/main/java/com/example/chris/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chris/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.chris.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,15 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
+    public void startFunction(View view) {
+        startActivity(new Intent(MainActivity.this, MapsActivity.class));
+    }
 }
