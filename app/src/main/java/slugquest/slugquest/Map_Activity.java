5914e7b8ec3769f1455677004f426d0328a964cd
SolSearcher <r433scriven@gmail.com>
2018-05-05 13:46:07 -0700

    Revert "Current location (from dustin)"
    This reverts commit 8acc12e808dda0f8f1c657bbd27d282e6d513598.

--- app/src/main/java/slugquest/slugquest/Map_Activity.java
+++ app/src/main/java/slugquest/slugquest/Map_Activity.java
@@ -10,29 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-// from dustin
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.content.Intent;
-import android.content.Context;
-import android.util.Log;
-import android.widget.TextView;
-import android.location.Location;
-import android.location.LocationManager;
-import android.view.View;
-import java.util.List;
-import android.widget.Button;
-
 public class Map_Activity extends FragmentActivity implements OnMapReadyCallback {
-
-    private final String TAG = "TESTGPS";
 
     private GoogleMap mMap;
 
@@ -64,82 +42,5 @@ public class Map_Activity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    // This statement requests permission to the user.
-    // If permissions are not set in the Manifest file, then access
-    // will automatically be denied. Once the user chooses an option,
-    // onRequestPermissionsResult is called.
-//        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},99);
-
-//    Button b = findViewById(R.id.button);
-//        b.setOnClickListener(new Button.OnClickListener(){
-//        @SuppressLint("MissingPermission")
-//        public void onClick(View view){
-//            // A reference to the location manager. The LocationManager has already
-//            // been set up in MyService, we're just getting a reference here.
-//            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//            List<String> providers = lm.getProviders(true);
-//            Location l;
-//            // Go through the location providers starting with GPS, stop as soon
-//            // as we find one.
-//            for (int i=providers.size()-1; i>=0; i--) {
-//                l = lm.getLastKnownLocation(providers.get(i));
-//                longText.setText(Double.toString(l.getLongitude()));
-//                latText.setText(Double.toString(l.getLatitude()));
-//                if (l != null) break;
-//            }
-//        }
-//    });
-//}
-    // This class implements OnRequestPermissionsResultCallback, so when the
-    // user is prompted for location permission, the below method is called
-    // as soon as the user chooses an option.
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-//        Log.d(TAG, "callback");
-        switch (requestCode) {
-            case 99:
-                // If the permissions aren't set, then return. Otherwise, proceed.
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-                                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
-                                , 10);
-                    }
-//                    Log.d(TAG, "returning program");
-                    return;
-                }
-                else{
-                    // Create Intent to reference MyService, start the Service.
-//                    Log.d(TAG, "starting service");
-                    Intent i = new Intent(this, MyService.class);
-                    if(i==null)
-                        Log.d(TAG, "intent null");
-                    else{
-                        startService(i);
-                    }
-
-                }
-                break;
-            default:
-                break;
-        }
-    }
-    // Used for debugging. Below method is extraneous
-    @SuppressLint("MissingPermission")
-    public void doSomething(View view){
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        List<String> providers = lm.getProviders(true);
-        Location l;
-        for (int i=providers.size()-1; i>=0; i--) {
-            l = lm.getLastKnownLocation(providers.get(i));
-//            longText.setText(Double.toString(l.getLongitude()));
-//            latText.setText(Double.toString(l.getLatitude()));
-            if (l != null) break;
-        }
     }
 }
