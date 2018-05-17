77fd98cc3a9478b038561002f5c971db9a3b250f
Fangee <hughelv@gmail.com>
2018-05-16 18:10:37 -0700

    Added Settings/Credits Screen

--- app/src/main/java/slugquest/slugquest/TitleActivity.java
+++ app/src/main/java/slugquest/slugquest/TitleActivity.java
@@ -15,10 +15,26 @@ public class TitleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_title);
 
         Button startButton = findViewById(R.id.startButton);
+        Button settingsButton = findViewById(R.id.settingsButton);
+        Button creditsButton = findViewById(R.id.creditsButton);
 
         startButton.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
                 Intent i = new Intent(TitleActivity.this, Map_Activity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+        settingsButton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(TitleActivity.this, SettingsActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+        creditsButton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(TitleActivity.this, CreditsActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
             }
