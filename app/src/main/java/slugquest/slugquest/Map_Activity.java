32973719139c601cfc97e42e02d33ce71b9bd430
nleeds <nleeds@ucsc.edu>
2018-05-16 18:21:50 -0700

    added example geofence in sydney and fuction to check is point is inside geofence

--- app/src/main/java/slugquest/slugquest/Map_Activity.java
+++ app/src/main/java/slugquest/slugquest/Map_Activity.java
@@ -2,6 +2,7 @@ package slugquest.slugquest;
 
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -15,6 +16,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -58,6 +61,15 @@ public class Map_Activity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMyLocationEnabled(true);
         //mMap.setOnMyLocationButtonClickListener(this);
         //mMap.setOnMyLocationClickListener(this);
+
+        //example of geofence area
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(-33.87365, 151.20689))
+                .radius(10000)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
+        Toast.makeText(Map_Activity.this,"setMyLocationEnabled",Toast.LENGTH_SHORT).show();
+
     }
 
     //Permission request
@@ -102,5 +114,27 @@ public class Map_Activity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(this,"Access denied",Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    // check if point is inside geofence using the Haversine formula
+    // https://stackoverflow.com/questions/30719757/is-there-any-api-for-calculating-geofence-breach-other-than-android-apis
+    boolean checkInside(Circle circle, double longitude, double latitude) {
+        return calculateDistance(
+                circle.getCenter().longitude, circle.getCenter().latitude, longitude, latitude
+        ) < circle.getRadius();}
+
+
+    double calculateDistance(
+            double longitude1, double latitude1,
+            double longitude2, double latitude2) {
+        double c =
+                Math.sin(Math.toRadians(latitude1)) *
+                        Math.sin(Math.toRadians(latitude2)) +
+                        Math.cos(Math.toRadians(latitude1)) *
+                                Math.cos(Math.toRadians(latitude2)) *
+                                Math.cos(Math.toRadians(longitude2) -
+                                        Math.toRadians(longitude1));
+        c = c > 0 ? Math.min(1, c) : Math.max(-1, c);
+        return 3959 * 1.609 * 1000 * Math.acos(c);
     }
 }
