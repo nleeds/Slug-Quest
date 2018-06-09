57146d54639f8763ef28f2c652df31385cf6944f
nleeds <nleeds@ucsc.edu>
2018-06-08 19:17:45 -0700

    added pictures and events

--- app/src/main/java/slugquest/slugquest/Globals.java
+++ app/src/main/java/slugquest/slugquest/Globals.java
@@ -12,9 +12,32 @@ public class Globals extends Application{
     //public Event[] eventArray;
 
     //Declares all events
+    //TODO: replace with actual values
+    double unknownlat = 36.994803;
+    double unknownlng = -122.067737;
+
+    //
     private Event[] eventArray= {
             new Event("Porter Meadows",36.994803, -122.067737,"sunset",1),
-            new Event("House",36.976465, -122.054937,"imagetest",0),
+            new Event("amphitheatre",unknownlat, unknownlng,"amphitheatre",2),
+            new Event("bookstore rock",unknownlat, unknownlng,"bookstorerock",3),
+            new Event("c10 mural",unknownlat, unknownlng,"c10mural",4),
+            new Event("House",36.976465, -122.054937,"imagetest",5),
+            new Event("carousel",unknownlat, unknownlng,"carousel",6),
+            new Event("chaotic pendulum",36.998669, -122.059941,"chaoticpendulum",7),
+            new Event("cowel fountain",unknownlat, unknownlng,"cowelfountain",8),
+            new Event("deathly hallows",unknownlat, unknownlng,"deathlyhallows",9),
+            new Event("dumpster art",37.000304, -122.058739,"dumpsterart",10),
+            new Event("happy lamp",36.993012, -122.063405,"happylamp",11),
+            new Event("media theatre seat",36.995894, -122.061044, "mediatheatreseat",12),
+            new Event("rock stairs",unknownlat, unknownlng,"rockstairs",13),
+            new Event("slug tunnel",unknownlat, unknownlng,"slugtunnel",14),
+            new Event("stevenson bench",unknownlat, unknownlng,"stevensonbench",15),
+            new Event("stevenson bust",unknownlat, unknownlng,"stevensonbust",16),
+            new Event("the maternal tree",unknownlat, unknownlng,"thematernaltree",17),
+            new Event("tree in building",unknownlat, unknownlng,"treeinbuilding",18),
+            new Event("wood art",36.998175, -122.061495,"woodart",19),
+            new Event("seal sculpture",36.998244, -122.061368,"sealsculpture",0),
     };
 
     public Event getEvent(){
