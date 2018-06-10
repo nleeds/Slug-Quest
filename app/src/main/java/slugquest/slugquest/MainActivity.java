e1a42daf7d4174ccbcf813be9f5436bcb70bb0e9
Chris Kong <cxkong@ucsc.edu>
2018-06-09 17:01:39 -0700

    Splash Screen

---
+++ app/src/main/java/slugquest/slugquest/MainActivity.java
@@ -0,0 +1,34 @@
+package slugquest.slugquest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, TitleActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
--- app/src/main/java/slugquest/slugquest/TitleActivity.java
+++ app/src/main/java/slugquest/slugquest/TitleActivity.java
@@ -1,6 +1,7 @@
 package slugquest.slugquest;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
