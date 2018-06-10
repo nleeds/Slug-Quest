97168dec8a1e4a29e463cbd446d8dfc00a94bfe9
nleeds <nleeds@ucsc.edu>
2018-06-09 17:26:04 -0700

    removed toolbar from splash screen

--- app/src/main/java/slugquest/slugquest/MainActivity.java
+++ app/src/main/java/slugquest/slugquest/MainActivity.java
@@ -3,8 +3,6 @@ package slugquest.slugquest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -24,9 +22,6 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_TIME_OUT);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
     }
