2644e68149d7f984f17a69752410e212436dcec0
SolSearcher <r433scriven@gmail.com>
2018-06-11 12:53:56 -0700

    tutorial

--- app/src/main/java/slugquest/slugquest/InstructionsActivity.java
+++ app/src/main/java/slugquest/slugquest/InstructionsActivity.java
@@ -2,11 +2,14 @@ package slugquest.slugquest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class InstructionsActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
             setContentView(R.layout.activity_instructions);
         }
 }
